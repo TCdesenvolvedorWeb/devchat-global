7fcf1a3ed386521bda2645a5ad0f03db220bb89d
Kalyel Mendes <kalyelmendesdapaixao@yahoo.com.br>
2025-02-01 17:52:29 -0300

    add funcoes

--- src/index.js
+++ src/index.js
@@ -7,32 +7,54 @@ const price = document.querySelectorAll('.price');
 const yearlyOption = document.querySelectorAll('.yearly-option');
 const periodOption = document.querySelectorAll('.select-period-option');
 const plans = document.querySelectorAll('.plans');
+const inputPersonalInfo = document.querySelectorAll('.input-personal-info');
+const requiredMessage = document.querySelectorAll('.required-message');
 
 
 let formAtual = 0;
 
+function validateForm() {
+    const emptyInput = [...inputPersonalInfo].some(input => input.value === '');
+    if (emptyInput) {
+        validateInput();
+    } else {
+
+        const formAtivo = document.querySelector('.active');
+        formAtivo.classList.remove('active');
+        formAtual++;
+        form[formAtual].classList.add('active');
+
+        const stepActive = document.querySelector('.active-state');
+        stepActive.classList.remove('active-state');
+        stepPosition[formAtual].classList.add('active-state');
+
+        const btnBackDisable = document.querySelector('.disable');
+
+        if (btnBackDisable) {
+            btnBackDisable.classList.remove('disable');
+        }
+
+    }
+}
+
+function validateInput() {
+
+    inputPersonalInfo.forEach(input => {
+        if (input.value === '') {
+            input.classList.add('required');
+        } else {
+            input.classList.remove('required');
+        }
+    })
+
+}
 
 btnNext.addEventListener('click', () => {
-    
-    // if (form === form[1]) {
-    //     btnBack.classList.remove('disable');
-    // }
     if (formAtual === form.length - 1) return
 
-    const formAtivo = document.querySelector('.active');
-    formAtivo.classList.remove('active');
-    formAtual++;
-    form[formAtual].classList.add('active');
-
-    const stepActive = document.querySelector('.active-state');
-    stepActive.classList.remove('active-state');
-    stepPosition[formAtual].classList.add('active-state');
-    
-    const btnBackDisable = document.querySelector('.disable');
-
-    btnBackDisable.classList.remove('disable')
-    
+    validateForm();
 })
+
 
 btnBack.addEventListener('click', () => {
     if (formAtual === 0) return
@@ -49,7 +71,7 @@ btnBack.addEventListener('click', () => {
     if (formAtual === 0) {
         btnBack.classList.add('disable')
     }
-    
+    validateInput();
 })
 
 let contador = 0;
@@ -67,10 +89,10 @@ toggleButton.addEventListener('change', function () {
 
         console.log('ativou');
 
-        price.forEach( (e , index) => {
-            if(index === 0) e.innerHTML = '$90/yr';
-            if(index === 1) e.innerHTML = '$120/yr';
-            if(index === 2) e.innerHTML = '$150/yr';
+        price.forEach((e, index) => {
+            if (index === 0) e.innerHTML = '$90/yr';
+            if (index === 1) e.innerHTML = '$120/yr';
+            if (index === 2) e.innerHTML = '$150/yr';
         });
 
     } else {
@@ -84,10 +106,10 @@ toggleButton.addEventListener('change', function () {
         contador--;
         periodOption[contador].classList.add('selected');
 
-        price.forEach( (e , index) => {
-            if(index === 0) e.innerHTML = '$9/mo';
-            if(index === 1) e.innerHTML = '$12/mo';
-            if(index === 2) e.innerHTML = '$15/mo';
+        price.forEach((e, index) => {
+            if (index === 0) e.innerHTML = '$9/mo';
+            if (index === 1) e.innerHTML = '$12/mo';
+            if (index === 2) e.innerHTML = '$15/mo';
         });
         console.log('desativou');
 
@@ -98,16 +120,13 @@ plans.forEach(plan => {
     plan.addEventListener('click', (e) => {
         e.preventDefault()
 
-        if (plan.classList.contains('selected')) {
-            plan.classList.remove('selected')
-            return
-        } else {
-            plan.classList.add('selected');
-        }
-        
+        const planActuallyActive = document.querySelector('.selected');
+
+        planActuallyActive && planActuallyActive.classList.remove('selected');
+
+        plan.classList.toggle('selected');
+
     })
 })
-
-console.log(plans);
 
 
